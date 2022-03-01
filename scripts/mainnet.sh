97e7404c116c2c6ba8cc20cb5fe6e9e05eb945cc
Maks Pawlak <120831+m4ksio@users.noreply.github.com>
2022-02-28 23:41:20 -0800

    Add mainnet scripts

--- scripts/mainnet.sh
+++ scripts/mainnet.sh
@@ -1,20 +1,28 @@
 source "$(dirname "$0")/common.sh"
 
-live_data "/var/flow" "mainnet-15"  "https://storage.googleapis.com/flow-genesis-bootstrap/mainnet-15-execution" "c8d4c90a40ec74aaf408bd7205d533e8b1901016f54695cbd71e0be4cae8725a"
+DATA_DIR="/var/flow/dps/mainnet16"
+
+# bootstrap function should set those variables
+
+GCP_BUCKET=""
+SEED_ADDRESS=""
+SEED_KEY=""
+
+bootstrap "mainnet" 16 "$DATA_DIR"
 
 # make sure you build a docker image first
 # dir where data has been downloaded should be mounted
 docker run \
-  -v /var/flow:/data/ \
+  -v $DATA_DIR/:/data/ \
   -p 5005:5005 \
-  flow-dps-live:v0.23 \
+  gcr.io/flow-container-registry/flow-dps-live:v0.24 \
   -a 0.0.0.0:5005 \
-  -i /data/mainnet-15/index \
-  -b /data/mainnet-15/bootstrap \
-  -c /data/mainnet-15/root.checkpoint \
-  -d /data/mainnet-15/protocol \
+  -i /data/index \
+  -b /data/bootstrap \
+  -c /data/bootstrap/root.checkpoint \
+  -d /data/protocol \
   -l info \
-  -u flow_public_mainnet15_execution_state \
-  --seed-address="access-007.mainnet15.nodes.onflow.org:3569" \
-  --seed-key="28a0d9edd0de3f15866dfe4aea1560c4504fe313fc6ca3f63a63e4f98d0e295144692a58ebe7f7894349198613f65b2d960abf99ec2625e247b1c78ba5bf2eae"
+  -u $GCP_BUCKET \
+  --seed-address="$SEED_ADDRESS" \
+  --seed-key="$SEED_KEY"
 
